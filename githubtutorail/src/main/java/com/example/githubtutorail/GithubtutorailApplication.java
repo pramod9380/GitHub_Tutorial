48ef3298e3e259e9dfea7a078d05b8f8a5e0c2e8
pramod.k <pramod.k@perfios.com>
2023-06-29 21:36:21 +0530

    created a new project

---
+++ githubtutorail/src/main/java/com/example/githubtutorail/GithubtutorailApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubtutorail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubtutorailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubtutorailApplication.class, args);
+    }
+
+}
---
+++ githubtutorail/src/test/java/com/example/githubtutorail/GithubtutorailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubtutorail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubtutorailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
